bc5e1a2403361f38c1ef1529f6121f139553af03
Shamika Harshamal <117425350+ShamikaLK@users.noreply.github.com>
2026-01-04 08:26:34 +0530

    Dual Direction Pattern

--- LED Chaser/Dual_Direction_Pattern.ino
+++ LED Chaser/Dual_Direction_Pattern.ino
@@ -1,3 +1,4 @@
+// LED pins definition | LED පින් අර්ථ දැක්වීම
 int LED1 = 2;
 int LED2 = 3;
 int LED3 = 4;
@@ -10,12 +11,13 @@ int LED9 = 10;
 int LED10 = 11;
 int LED11 = 12;
 int LED12 = 13;
-int t=20;
-int y=100;
+int t=20; // Short delay time for chasing effect | පසුපස්සේ යන ආකාරය සඳහා කෙටි ප්‍රමාද කාලය
+int y=100; // Longer delay time for closing effect | වසා දැමීමේ ආකාරය සඳහා දිගු ප්‍රමාද කාලය
 
 
 
 void setup() {
+   // Set all LED pins as OUTPUT | සියලු LED පින් OUTPUT ලෙස සකසන්න
    pinMode(LED1, OUTPUT);
    pinMode(LED2, OUTPUT);
    pinMode(LED3, OUTPUT); 
@@ -27,12 +29,117 @@ void setup() {
    pinMode(LED9, OUTPUT);
    pinMode(LED10, OUTPUT);
    pinMode(LED11, OUTPUT);
-   pinMode(LED12, OUTPUT); 
+   pinMode(LED12, OUTPUT);
+    
 }
 
 
 void loop() {
-                    
+// First chasing pattern: Turn on LEDs one by one from right to left (LED12 to LED1) | පළමු පසුපස්සේ යන ආකාරය: LED එකින් එක දකුණේ සිට වමට (LED12 සිට LED1 දක්වා) අලුත් කරන්න                    
+  digitalWrite(LED12, HIGH);    
+  delay(t);                 
+  digitalWrite(LED11, HIGH);    
+  delay(t);                  
+  digitalWrite(LED10, HIGH); 
+  delay(t);                 
+  digitalWrite(LED9, HIGH);  
+  delay(t);                  
+  digitalWrite(LED8, HIGH);    
+  delay(t);                 
+  digitalWrite(LED7, HIGH);    
+  delay(t);                 
+  digitalWrite(LED6, HIGH);  
+  delay(t);               
+  digitalWrite(LED5, HIGH); 
+  delay(t);  
+  digitalWrite(LED4, HIGH);    
+  delay(t);
+  digitalWrite(LED3, HIGH);    
+  delay(t);
+  digitalWrite(LED2, HIGH);    
+  delay(t);
+  digitalWrite(LED1, HIGH);    
+  delay(t);
+   
+// Turn off LEDs one by one from right to left (LED12 to LED1) | LED එකින් එක දකුණේ සිට වමට (LED12 සිට LED1 දක්වා) නිවා දමන්න                    
+  digitalWrite(LED12, LOW);    
+  delay(t);                
+  digitalWrite(LED11, LOW);    
+  delay(t);                  
+  digitalWrite(LED10, LOW);   
+  delay(t);               
+  digitalWrite(LED9, LOW);     
+  delay(t);               
+  digitalWrite(LED8, LOW);  
+  delay(t);                 
+  digitalWrite(LED7, LOW);  
+  delay(t);                  
+  digitalWrite(LED6, LOW);     
+  delay(t);                  
+  digitalWrite(LED5, LOW);   
+  delay(t); 
+  digitalWrite(LED4, LOW);    
+  delay(t);
+  digitalWrite(LED3, LOW);    
+  delay(t);
+  digitalWrite(LED2, LOW);    
+  delay(t);
+  digitalWrite(LED1, LOW);    
+  delay(t);
+
+// Second chasing pattern: Turn on LEDs one by one from left to right (LED1 to LED12) | දෙවන පසුපස්සේ යන ආකාරය: LED එකින් එක වමේ සිට දකුණට (LED1 සිට LED12 දක්වා) අලුත් කරන්න
+  digitalWrite(LED1, HIGH);    
+  delay(t);
+  digitalWrite(LED2, HIGH);    
+  delay(t);
+  digitalWrite(LED3, HIGH);    
+  delay(t);
+  digitalWrite(LED4, HIGH);    
+  delay(t); 
+  digitalWrite(LED5, HIGH); 
+  delay(t);
+  digitalWrite(LED6, HIGH);  
+  delay(t);
+  digitalWrite(LED7, HIGH);  
+  delay(t);
+  digitalWrite(LED8, HIGH);  
+  delay(t);
+  digitalWrite(LED9, HIGH);  
+  delay(t);
+  digitalWrite(LED10, HIGH);  
+  delay(t);
+  digitalWrite(LED11, HIGH);  
+  delay(t);
+  digitalWrite(LED12, HIGH);  
+  delay(t);
+
+// Turn off LEDs one by one from left to right (LED1 to LED12) | LED එකින් එක වමේ සිට දකුණට (LED1 සිට LED12 දක්වා) නිවා දමන්න
+  digitalWrite(LED1, LOW);    
+  delay(t);
+  digitalWrite(LED2, LOW);    
+  delay(t);
+  digitalWrite(LED3, LOW);    
+  delay(t);
+  digitalWrite(LED4, LOW);    
+  delay(t);
+  digitalWrite(LED5, LOW);    
+  delay(t);
+  digitalWrite(LED6, LOW);    
+  delay(t);
+  digitalWrite(LED7, LOW);    
+  delay(t);
+  digitalWrite(LED8, LOW);    
+  delay(t);
+  digitalWrite(LED9, LOW);    
+  delay(t);
+  digitalWrite(LED10, LOW);    
+  delay(t);
+  digitalWrite(LED11, LOW);    
+  delay(t);
+  digitalWrite(LED12, LOW);    
+  delay(t);
+
+// Repeat first chasing pattern (right to left) | පළමු පසුපස්සේ යන ආකාරය නැවත කරන්න (දකුණේ සිට වමට)
   digitalWrite(LED12, HIGH);    
   delay(t);                 
   digitalWrite(LED11, HIGH);    
@@ -68,7 +175,7 @@ void loop() {
   digitalWrite(LED9, LOW);     
   delay(t);               
   digitalWrite(LED8, LOW);  
- delay(t);                 
+  delay(t);                 
   digitalWrite(LED7, LOW);  
   delay(t);                  
   digitalWrite(LED6, LOW);     
@@ -84,12 +191,13 @@ void loop() {
   digitalWrite(LED1, LOW);    
   delay(t);
 
+// Repeat second chasing pattern (left to right) | දෙවන පසුපස්සේ යන ආකාරය නැවත කරන්න (වමේ සිට දකුණට)
   digitalWrite(LED1, HIGH);    
-  delay(t);
+  delay(t); 
   digitalWrite(LED2, HIGH);    
-  delay(t);
+  delay(t); 
   digitalWrite(LED3, HIGH);    
-  delay(t);
+  delay(t); 
   digitalWrite(LED4, HIGH);    
   delay(t); 
   digitalWrite(LED5, HIGH); 
@@ -109,6 +217,8 @@ void loop() {
   digitalWrite(LED12, HIGH);  
   delay(t);
 
+
+// Turn off LEDs left to right again | LED නැවත වමේ සිට දකුණට නිවා දමන්න
   digitalWrite(LED1, LOW);    
   delay(t);
   digitalWrite(LED2, LOW);    
@@ -134,6 +244,7 @@ void loop() {
   digitalWrite(LED12, LOW);    
   delay(t);
 
+// Final repetition of first chasing pattern (right to left) | පළමු පසුපස්සේ යන ආකාරය අවසන් වතාවට නැවත කරන්න (දකුණේ සිට වමට)
   digitalWrite(LED12, HIGH);    
   delay(t);                 
   digitalWrite(LED11, HIGH);    
@@ -169,7 +280,7 @@ void loop() {
   digitalWrite(LED9, LOW);     
   delay(t);               
   digitalWrite(LED8, LOW);  
- delay(t);                 
+  delay(t);                 
   digitalWrite(LED7, LOW);  
   delay(t);                  
   digitalWrite(LED6, LOW);     
@@ -185,108 +296,7 @@ void loop() {
   digitalWrite(LED1, LOW);    
   delay(t);
 
-   digitalWrite(LED1, HIGH);    
-  delay(t); 
-  digitalWrite(LED2, HIGH);    
-  delay(t); 
-  digitalWrite(LED3, HIGH);    
-  delay(t); 
-  digitalWrite(LED4, HIGH);    
-  delay(t); 
-  digitalWrite(LED5, HIGH); 
-  delay(t);
-  digitalWrite(LED6, HIGH);  
-  delay(t);
-  digitalWrite(LED7, HIGH);  
-  delay(t);
-  digitalWrite(LED8, HIGH);  
-  delay(t);
-  digitalWrite(LED9, HIGH);  
-  delay(t);
-  digitalWrite(LED10, HIGH);  
-  delay(t);
-  digitalWrite(LED11, HIGH);  
-  delay(t);
-  digitalWrite(LED12, HIGH);  
-  delay(t);
-
-
-  digitalWrite(LED1, LOW);    
-  delay(t);
-  digitalWrite(LED2, LOW);    
-  delay(t);
-  digitalWrite(LED3, LOW);    
-  delay(t);
-  digitalWrite(LED4, LOW);    
-  delay(t);
-  digitalWrite(LED5, LOW);    
-  delay(t);
-  digitalWrite(LED6, LOW);    
-  delay(t);
-  digitalWrite(LED7, LOW);    
-  delay(t);
-  digitalWrite(LED8, LOW);    
-  delay(t);
-  digitalWrite(LED9, LOW);    
-  delay(t);
-  digitalWrite(LED10, LOW);    
-  delay(t);
-  digitalWrite(LED11, LOW);    
-  delay(t);
-  digitalWrite(LED12, LOW);    
-  delay(t);
-
-  digitalWrite(LED12, HIGH);    
-  delay(t);                 
-  digitalWrite(LED11, HIGH);    
-  delay(t);                  
-  digitalWrite(LED10, HIGH); 
-  delay(t);                 
-  digitalWrite(LED9, HIGH);  
-  delay(t);                  
-  digitalWrite(LED8, HIGH);    
-  delay(t);                 
-  digitalWrite(LED7, HIGH);    
-  delay(t);                 
-  digitalWrite(LED6, HIGH);  
-  delay(t);               
-  digitalWrite(LED5, HIGH); 
-  delay(t);  
-  digitalWrite(LED4, HIGH);    
-  delay(t);
-  digitalWrite(LED3, HIGH);    
-  delay(t);
-  digitalWrite(LED2, HIGH);    
-  delay(t);
-  digitalWrite(LED1, HIGH);    
-  delay(t);
-   
-                    
-  digitalWrite(LED12, LOW);    
-  delay(t);                
-  digitalWrite(LED11, LOW);    
-  delay(t);                  
-  digitalWrite(LED10, LOW);   
-  delay(t);               
-  digitalWrite(LED9, LOW);     
-  delay(t);               
-  digitalWrite(LED8, LOW);  
- delay(t);                 
-  digitalWrite(LED7, LOW);  
-  delay(t);                  
-  digitalWrite(LED6, LOW);     
-  delay(t);                  
-  digitalWrite(LED5, LOW);   
-  delay(t); 
-  digitalWrite(LED4, LOW);    
-  delay(t);
-  digitalWrite(LED3, LOW);    
-  delay(t);
-  digitalWrite(LED2, LOW);    
-  delay(t);
-  digitalWrite(LED1, LOW);    
-  delay(t);
-
+// Final repetition of second chasing pattern (left to right) | දෙවන පසුපස්සේ යන ආකාරය අවසන් වතාවට නැවත කරන්න (වමේ සිට දකුණට)
   digitalWrite(LED1, HIGH);    
   delay(t);
   digitalWrite(LED2, HIGH);    
@@ -312,6 +322,7 @@ void loop() {
   digitalWrite(LED12, HIGH);  
   delay(t);
 
+// Turn off all LEDs after final chase | අවසන් පසුපස්සේ යන ආකාරයෙන් පසු සියලු LED නිවා දමන්න
   digitalWrite(LED1, LOW);    
   delay(t);
   digitalWrite(LED2, LOW);    
@@ -337,73 +348,54 @@ void loop() {
   digitalWrite(LED12, LOW);    
   delay(t);
 
+// Turn on all LEDs simultaneously for 3 seconds | සියලු LED එකවර 3 තත්පරයක් අලුත් කරන්න
   digitalWrite(LED1, HIGH); 
-
   digitalWrite(LED2, HIGH); 
-
   digitalWrite(LED3, HIGH); 
-
   digitalWrite(LED4, HIGH);    
-    
   digitalWrite(LED5, HIGH); 
-   
   digitalWrite(LED6, HIGH);  
-   
   digitalWrite(LED7, HIGH);  
-   
   digitalWrite(LED8, HIGH);  
-   
   digitalWrite(LED9, HIGH);  
-   
   digitalWrite(LED10, HIGH);  
-   
   digitalWrite(LED11, HIGH);  
-   
   digitalWrite(LED12, HIGH);  
   delay(3000);
 
-    digitalWrite(LED12, LOW);
-    digitalWrite(LED1, LOW);
-    delay(y);
-    digitalWrite(LED11, LOW);
-    digitalWrite(LED2, LOW);
-    delay(y);
-    digitalWrite(LED10, LOW);
-    digitalWrite(LED3, LOW);
-    delay(y);
-    digitalWrite(LED9, LOW);
-    digitalWrite(LED4, LOW);
-    delay(y);
-    digitalWrite(LED8, LOW);
-    digitalWrite(LED5, LOW);
-    delay(y);
-    digitalWrite(LED7, LOW);
-    digitalWrite(LED6, LOW);
-    delay(y);
-   
+// Closing effect: Turn off LEDs in pairs from outside to inside | වසා දැමීමේ ආකාරය: LED යුගල වශයෙන් පිටතේ සිට ඇතුළට නිවා දමන්න
+  digitalWrite(LED12, LOW);
   digitalWrite(LED1, LOW);
-
+  delay(y);
+  digitalWrite(LED11, LOW);
   digitalWrite(LED2, LOW);
-
+  delay(y);
+  digitalWrite(LED10, LOW);
   digitalWrite(LED3, LOW);
-  
+  delay(y);
+  digitalWrite(LED9, LOW);
+  digitalWrite(LED4, LOW);
+  delay(y);
+  digitalWrite(LED8, LOW);
+  digitalWrite(LED5, LOW);
+  delay(y);
+  digitalWrite(LED7, LOW);
+  digitalWrite(LED6, LOW);
+  delay(y);
+   
+// Ensure all LEDs are off | සියලු LED නිවී ඇති බවට වග බලා ගන්න
+  digitalWrite(LED1, LOW);
+  digitalWrite(LED2, LOW);
+  digitalWrite(LED3, LOW);
   digitalWrite(LED4, LOW);    
-   
   digitalWrite(LED5, LOW);    
-   
   digitalWrite(LED6, LOW);    
-   
   digitalWrite(LED7, LOW);    
-   
   digitalWrite(LED8, LOW);    
-   
   digitalWrite(LED9, LOW);    
-   
   digitalWrite(LED10, LOW);    
-   
   digitalWrite(LED11, LOW);    
-   
   digitalWrite(LED12, LOW); 
-  delay(2000);   
+  delay(2000);   // Final pause before repeating the loop | ලූපය නැවත ආරම්භ කිරීමට පෙර අවසාන විරාමය
    
 }
